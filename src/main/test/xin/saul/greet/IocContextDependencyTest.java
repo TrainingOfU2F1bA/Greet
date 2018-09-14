c6e38f78ac13ea10b3aac898898e98df97e963e3
Kuan Xin <kxin@thoughtworks.com>
2018-09-14 17:48:35 +0800

    [Kuan]:refactor(IocContextNewRegisterBeanTest):pass test_should_create_class_with_dependency_with_had_not_been_register test case

--- src/main/test/xin/saul/greet/IocContextDependencyTest.java
+++ src/main/test/xin/saul/greet/IocContextDependencyTest.java
@@ -1,6 +1,7 @@
 package xin.saul.greet;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import xin.saul.greet.testclass.ClassWithDependency;
 import xin.saul.greet.testclass.DependencyClass;
 
@@ -19,4 +20,15 @@ public class IocContextDependencyTest {
 
     }
 
+    @Test
+    void test_should_create_class_with_dependency_with_had_not_been_register() throws InstantiationException, IllegalAccessException {
+
+        IocContextImpl iocContext = new IocContextImpl();
+        iocContext.registerBean(ClassWithDependency.class);
+        Executable executable= () -> {
+            ClassWithDependency bean = iocContext.getBean(ClassWithDependency.class);
+        };
+
+        assertThrows(IllegalStateException.class, executable);
+    }
 }
